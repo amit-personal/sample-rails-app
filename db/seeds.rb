61f24a7353de991b5517dd7dbffed700b272c5b0
Amit <ak9835371292@gmail.com>
2019-03-02 17:46:39 +0530

    added admin details for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,3 +39,6 @@ end
 
 PopulateDb.seed_db
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+PopulateDb.seed_db
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.production?
